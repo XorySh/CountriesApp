8723cf237446fec01953cebd91d7d04f435169ae
XorySh <melchordiazreyes@gmail.com>
2023-07-22 17:42:56 +0200

    Buscar por region optimizado

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,14 +13,17 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   public isLoading: boolean = false;
 
   constructor( private countriesService: CountriesService ){}
 
-  searchByRegion ( term: string ): void {
+  searchByRegion ( region: Region ): void {
+    this.selectedRegion = region;
     this.isLoading = true;
-    this.countriesService.searchRegion( term )
+    this.countriesService.searchRegion( region )
       .subscribe( countries => {
         this.isLoading = false;
         this.countries = countries;
